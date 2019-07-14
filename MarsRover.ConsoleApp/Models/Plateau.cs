37858a298a8f1568ca8afa7de722ae73aebc29d7
Ferhat <candasferhat61@gmail.com>
2019-07-14 20:51:02 +0300

    Plateau created

---
+++ MarsRover.ConsoleApp/Models/Plateau.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover.ConsoleApp.Models
+{
+    public class Plateau
+    {
+        /// <summary>
+        /// top right corner coordinate's
+        /// </summary>
+        private Coordinate TopRightCoordinate = new Coordinate(0, 0);
+        /// <summary>
+        /// botoomleft coordinate's
+        /// </summary>
+        private Coordinate BottomLeftCoordinate = new Coordinate(0, 0);
+        public Plateau(int topRightXCoordinate, int topRightYCoordinate) => this.TopRightCoordinate = this.TopRightCoordinate.NewCoordinateForStepSize(topRightXCoordinate, topRightYCoordinate);
+        /// <summary>
+        /// defined coordinate checks for current plateau has in bounds
+        /// </summary>
+        /// <param name="coordinate"></param>
+        /// <returns></returns>
+        public bool HasWithinBounds(Coordinate coordinate) => this.BottomLeftCoordinate.HasOutsideBounds(coordinate) && this.TopRightCoordinate.HasWithinBounds(coordinate);
+    }
+}
