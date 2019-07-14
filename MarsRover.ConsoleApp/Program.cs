c303d13bbd5e40d18917e95e41d7080c9c25b4d1
Ferhat <candasferhat61@gmail.com>
2019-07-14 20:52:02 +0300

    Update Program.cs

--- MarsRover.ConsoleApp/Program.cs
+++ MarsRover.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MarsRover.ConsoleApp.Models;
+using System;
 
 namespace MarsRover.ConsoleApp
 {
@@ -6,7 +7,21 @@ namespace MarsRover.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Plateau plateau = new Plateau(5, 5);
+            Coordinate coordinate = new Coordinate(1, 2);
+            Rover rover1 = new Rover(plateau,Enums.DirectionEnum.North, coordinate);
+            rover1.Run("LMLMLMLMM");
+            // 1 3 N
+            Console.WriteLine(rover1.CurrentLocation);
+
+
+            Coordinate coordinate2 = new Coordinate(3,3);
+            Rover rover2 = new Rover(plateau, Enums.DirectionEnum.East, coordinate2);
+            rover2.Run("MMRMMRMRRM");
+            //5 1 E
+            Console.WriteLine(rover2.CurrentLocation);
+
+            Console.ReadKey();
         }
     }
 }
