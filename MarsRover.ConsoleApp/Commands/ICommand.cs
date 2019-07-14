4a1c7fdc430ad837e393ba6dc31b67820f48392e
Ferhat <candasferhat61@gmail.com>
2019-07-14 20:51:51 +0300

    commands created

---
+++ MarsRover.ConsoleApp/Commands/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using MarsRover.ConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover.ConsoleApp.Commands
+{
+    public interface ICommand
+    {
+        void Execute(Rover rover);
+    }
+}
---
+++ MarsRover.ConsoleApp/Commands/RotateLeftCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MarsRover.ConsoleApp.Models;
+
+namespace MarsRover.ConsoleApp.Commands
+{
+    public class RotateLeftCommand : ICommand
+    {
+        public void Execute(Rover rover) => rover.TurnLeft();
+    }
+}
