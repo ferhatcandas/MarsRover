c0ca2605414751a3cc89b5c56e23e93837bfbaed
Ferhat <candasferhat61@gmail.com>
2019-07-14 20:51:34 +0300

    Create CoordinatesOutOfRangeException.cs

---
+++ MarsRover.ConsoleApp/Exception/CoordinatesOutOfRangeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover.ConsoleApp.Exception
+{
+    public class CoordinatesOutOfRangeException : System.Exception
+    {
+        public CoordinatesOutOfRangeException() : base("Entered coordinates is out of plateau.")
+        {
+
+        }
+    }
+}
